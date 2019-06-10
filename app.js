8a7e47d3d5b46716b182f8d85dd935f0c093fb02
Augusto <augusto.juliogec@gmail.com>
2019-06-10 11:12:13 -0300

    refactoring portal de noticias project with EJS

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ var app = express(); //express module returns a function, here we are executing 
 
 app.set('view engine', 'ejs'); //here we say that ejs will take care of our views
 
-app.get('/tecnologia', function(req, res){
-    res.render("secao/tecnologia") //here I can hide the file extension, because ejs can understand it
+app.get('/', function(req, res){
+    res.render("home/index"); //here I can hide the file extension, because ejs can understand it
 });
 
-app.get('/', function(req, res){
-    res.send("<html><body>Portal de Notícias</body></html>")
+app.get('/formulario_inclusao_noticia', function(req, res){
+    res.render("admin/form_add_noticia");
+});
+
+app.get('/noticias', function(req, res){
+    res.render("noticias/noticias");
 });
 
 app.listen(3000, function(){
