881ff11f4f370ed06ada215e2f8078b7d5f0bfa5
Augusto <augusto.juliogec@gmail.com>
2019-06-10 10:54:23 -0300

    installing EJS - template view engine

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express'); //retrieving express lib
 var app = express(); //express module returns a function, here we are executing this function
 
-app.get('/', function(req, res){
-    res.send("<html><body>Portal de Notícias</body></html>")
-});
+app.set('view engine', 'ejs'); //here we say that ejs will take care of our views
 
 app.get('/tecnologia', function(req, res){
-    res.send("<html><body>Notícias de tecnologia</body></html>")
+    res.render("secao/tecnologia") //here I can hide the file extension, because ejs can understand it
+});
+
+app.get('/', function(req, res){
+    res.send("<html><body>Portal de Notícias</body></html>")
 });
 
 app.listen(3000, function(){
