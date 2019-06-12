b8c188192c1bdbf5fac8ee35b6ad7eb86b628535
Augusto <augusto.juliogec@gmail.com>
2019-06-12 17:16:46 -0300

    creating MySQL connection and retrieving data from MySQL DB

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,6 +1,21 @@
 module.exports = function(app){
+
     app.get('/noticias', function(req, res){
-        res.render("noticias/noticias");
+
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+            host : 'localhost',
+            user : 'root',
+            password: 'root',
+            database : 'portal_noticias'
+        });
+
+        connection.query('select * from noticias', function(error, result){ //connection.query(<sql>,<func callback>)
+            res.send(result);
+        });
+    
+        //res.render("noticias/noticias");
     });
 };  
     
