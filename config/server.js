1acab9fcc0ad6bf61d9e1752378c0b380e1776f8
Augusto Julio <augusto.juliogec@gmail.com>
2019-06-15 19:40:55 -0300

    refactoring project portal de noticias with consign (manage modules - routes)

--- config/server.js
+++ config/server.js
@@ -1,6 +1,10 @@
 var express = require('express'); //retrieving express lib
+var consign = require('consign'); //retrieving consign lib
+
 var app = express(); //express module returns a function, here we are executing this function
 app.set('view engine', 'ejs'); //here we say that ejs will take care of our views
 app.set('views', './app/views'); //here we say "where are the views"
+
+consign().include('app/routes').into(app); //scan routes folder and include these routes to our server (app)
 
 module.exports = app;
