02340a98e4ec4702b96e2e432247034046e8c7a4
Augusto <augusto.juliogec@gmail.com>
2019-06-18 10:10:54 -0300

    installing express-validator

--- config/server.js
+++ config/server.js
@@ -1,12 +1,14 @@
 var express = require('express'); //retrieving express lib
 var consign = require('consign'); //retrieving consign lib
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 var app = express(); //express module returns a function, here we are executing this function
 app.set('view engine', 'ejs'); //here we say that ejs will take care of our views
 app.set('views', './app/views'); //here we say "where are the views"
 
 app.use(bodyParser.urlencoded({extended: true})) //the body-parser is a middleware
+app.use(expressValidator());
 
 consign()               
     .include('app/routes') //scan routes folder and include these routes to our server (app)
