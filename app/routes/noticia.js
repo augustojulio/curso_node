4306cee851b6120fe9daff33b64c0fe8cad54e40
Augusto <augusto.juliogec@gmail.com>
2019-06-18 10:00:00 -0300

    refactoring news portal project - implementing classes

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -3,9 +3,9 @@ module.exports = function(app){
     app.get('/noticia', function(req, res){
 
         var connection = app.config.dbConnection();
-        var noticiasModel = app.app.models.noticiasModel;
+        var noticiasModel = new app.app.models.NoticiasDAO(connection);
 
-        noticiasModel.getNoticia(connection,  function(error, result){ //connection.query(<sql>,<func callback>)
+        noticiasModel.getNoticia(function(error, result){ //connection.query(<sql>,<func callback>)
             res.render("noticias/noticia", {noticia : result});
         });    
     });
