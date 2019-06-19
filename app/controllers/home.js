439149921c1fb77e16f1abc9bce471f44761bd08
Augusto <augusto.juliogec@gmail.com>
2019-06-19 16:05:54 -0300

    replicating backend logic

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,10 @@
 module.exports.index = function(application, req, res){
-    res.render("home/index"); //here I can hide the file extension, because ejs can understand it
+    var connection = application.config.dbConnection();
+
+    var noticiasModel = new application.app.models.NoticiasDAO(connection);
+
+    noticiasModel.get5UltimasNoticias(function(error, result){
+        res.render("home/index", {noticias : result});
+    });
+
 }
