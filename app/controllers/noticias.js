089eef0614ab771f6adf17861991fbf9c8c09334
Augusto Julio <augusto.juliogec@gmail.com>
2019-06-20 15:03:59 -0300

    sending and receiving params

--- app/controllers/noticias.js
+++ app/controllers/noticias.js
@@ -11,7 +11,9 @@ module.exports.noticia = function(application, req, res){
     var connection = application.config.dbConnection();
     var noticiasModel = new application.app.models.NoticiasDAO(connection);
 
-    noticiasModel.getNoticia(function(error, result){ //connection.query(<sql>,<func callback>)
+    var id_noticia = req.query;
+
+    noticiasModel.getNoticia(id_noticia, function(error, result){ //connection.query(<sql>,<func callback>)
         res.render("noticias/noticia", {noticia : result});
     }); 
 }
