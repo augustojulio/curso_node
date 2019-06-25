07032bb6d690f6b6a214f8968149ac152988b0b6
Augusto Julio <augusto.juliogec@gmail.com>
2019-06-25 12:22:27 -0300

    app listening to protocols - http and socketio

--- project2_multiroom_chat/app.js
+++ project2_multiroom_chat/app.js
@@ -3,6 +3,8 @@
 var app = require('./config/server');
 
 /* parameterize the listening port */
-app.listen(8080, function(){
+var server = app.listen(8080, function(){
     console.log("Server online");
 });
+
+require('socket.io').listen(server);
