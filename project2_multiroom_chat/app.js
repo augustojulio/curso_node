a0c07adf5ef4eba3f574a8b2b7fcd66cc53e24a0
Augusto Julio <augusto.juliogec@gmail.com>
2019-06-30 19:01:39 -0300

    updating people in a chat and end of project 2 multichat room

--- project2_multiroom_chat/app.js
+++ project2_multiroom_chat/app.js
@@ -20,6 +20,8 @@ io.on('connection', function(socket){ //callback of connection object
     });
 
     socket.on('msgParaServidor', function(data){
+
+    	/*dialog*/
     	socket.emit(
     		'msgParaCliente',
     		 { apelido: data.apelido, mensagem: data.mensagem}
@@ -29,5 +31,18 @@ io.on('connection', function(socket){ //callback of connection object
     		'msgParaCliente',
     		 { apelido: data.apelido, mensagem: data.mensagem}
     	);
+
+    	/*participants*/
+    	if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+	    	socket.emit(
+	    		'participantesParaCliente',
+	    		 { apelido: data.apelido}
+	    	);
+
+	    	socket.broadcast.emit(
+	    		'participantesParaCliente',
+	    		 { apelido: data.apelido}
+	    	);
+	    }
     });
 });
