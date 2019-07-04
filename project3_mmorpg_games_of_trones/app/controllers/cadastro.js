fab4d71560806a576b82d6ddae46321eab7dfe6f
Augusto Julio <augusto.juliogec@gmail.com>
2019-07-03 21:44:45 -0300

    user registration - receiving and validating data

--- project3_mmorpg_games_of_trones/app/controllers/cadastro.js
+++ project3_mmorpg_games_of_trones/app/controllers/cadastro.js
@@ -1,3 +1,24 @@
 module.exports.cadastro = function(application, req, res){
-	res.render('cadastro');
+	res.render('cadastro', { validacao: {}, dadosForm: {}});
+}
+
+
+module.exports.cadastrar = function(application, req, res){
+
+	var dadosForm = req.body;
+
+	req.assert('nome', 'Nome não pode ser vazio').notEmpty();
+	req.assert('usuario', 'Usuário não deve ser vazio').notEmpty();
+	req.assert('senha', 'Senha não deve ser vazia').notEmpty();
+	req.assert('casa', 'Casa não deve ser vazia').notEmpty();
+
+	var erros = req.validationErrors();
+
+	if(erros){
+		res.render('cadastro', { validacao: erros, dadosForm: dadosForm});
+		return;
+	}
+
+	res.send('Podemos cadastrar');
+
 }
