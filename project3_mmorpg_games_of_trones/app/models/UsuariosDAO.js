3451f68e5c78d693e18adf42eb6cff70c65159d6
Augusto Julio <augusto.juliogec@gmail.com>
2019-07-05 16:16:43 -0300

    encrypting user passwords with MD5

--- project3_mmorpg_games_of_trones/app/models/UsuariosDAO.js
+++ project3_mmorpg_games_of_trones/app/models/UsuariosDAO.js
@@ -1,3 +1,6 @@
+/*import crypto module*/ 
+var crypto = require("crypto");
+
 function UsuariosDAO(connection){
 	this._connection = connection();
 
@@ -6,6 +9,11 @@ function UsuariosDAO(connection){
 UsuariosDAO.prototype.inserirUsuario = function(usuario){
 	this._connection.open( function(err, mongoclient){
 		mongoclient.collection("usuarios", function(err, collection){
+
+			var senha_criptografada = crypto.createHash("md5").update(usuario.senha).digest("hex");
+
+			usuario.senha = senha_criptografada;
+
 			collection.insert(usuario);
 
 			mongoclient.close();
@@ -16,6 +24,10 @@ UsuariosDAO.prototype.inserirUsuario = function(usuario){
 UsuariosDAO.prototype.autenticar = function(usuario, req, res){
 	this._connection.open( function(err, mongoclient){
 		mongoclient.collection("usuarios", function(err, collection){
+
+			var senha_criptografada = crypto.createHash("md5").update(usuario.senha).digest("hex");
+			usuario.senha = senha_criptografada;
+			
 			collection.find(usuario).toArray(function(err, result){ //collection.find({usuario: {$eq: usuario.usuario}, senha: {$eq: usuario.senha}});
 				
 				if(result[0] != undefined){
