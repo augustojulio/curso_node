6652388cb1cb416636ae84de6d5eabe8327efeea
Augusto Julio <augusto.juliogec@gmail.com>
2019-07-10 10:25:36 -0300

    including docs on mongodb with POST

--- project4_instagram_clone/api/server.js
+++ project4_instagram_clone/api/server.js
@@ -12,10 +12,35 @@ var port = 8080;
 
 app.listen(port);
 
+var db = new mongodb.Db(
+	'instagram',
+	new mongodb.Server('localhost', 27017, {}),
+	{}
+);
+
 console.log('HTTP server is listening port' + port);
 
 app.get('/', function(req, res){
-	
+
 	res.send({ msg: 'olá'});
 
+});
+
+app.post('/api', function(req, res){
+
+	var dados = req.body;
+
+	db.open(function(err, mongoclient){
+		mongoclient.collection('postagens', function(err, collection){
+			collection.insert(dados, function(err, records){
+				if(err){
+					res.json({'status' : 'erro'});
+				} else {
+					res.json({'status' : 'inclusao realizada com sucesso'});
+				}
+				mongoclient.close();
+			});
+		});
+
+	});
 });
